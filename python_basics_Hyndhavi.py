c5780fdd16c0b0718c4a27ae72c137048ac82461
Hyndhavi5169 <47411124+Hyndhavi5169@users.noreply.github.com>
2026-04-16 02:36:42 +0530

    Add files via upload

---
+++ python_basics_Hyndhavi.py
@@ -0,0 +1,16 @@
+
+#Task-1
+Name = "Hyndhavi"
+Age = 25
+Height = 1.53
+Student = True
+print(f"{Name}, {Age}, {Height}, {Student}")
+
+#Task-2
+print(f"Name: {Name} | Age: {Age} | Height: {Height} | Student: {Student}")
+
+#Task-3
+print (f"Age in months: {Age*12}")
+print (f"Age in days: {Age*365}")
+print (f"The remainder when age is divided by 7: {Age % 7}")
+print(f"Age raised to the power of 2: {Age ** 2}")
