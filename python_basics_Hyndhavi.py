07c7cf28fd13bcf0ba71274e82de5e63e7e91147
Hyndhavi5169 <47411124+Hyndhavi5169@users.noreply.github.com>
2026-04-16 02:23:34 +0530

    Created using Colab

---
+++ python_basics_Hyndhavi.py
@@ -0,0 +1,118 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMUkOwDS3z1nV79j25xdM17",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Hyndhavi5169/Hyndhavi_AIML_Learning/blob/main/python_basics_Hyndhavi.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 6,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "Sx57GuP-2uEI",
+        "outputId": "18752f89-483a-45ca-9528-798dbe485ddf"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Hyndhavi, 25, 1.53, True\n"
+          ]
+        }
+      ],
+      "source": [
+        "Name = \"Hyndhavi\"\n",
+        "Age = 25\n",
+        "Height = 1.53\n",
+        "Student = True\n",
+        "print(f\"{Name}, {Age}, {Height}, {Student}\")"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "print(f\"Name: {Name} | Age: {Age} | Height: {Height} | Student: {Student}\")"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "iHMQy68X491I",
+        "outputId": "6e8317b7-9074-49ff-887b-3ef3ad3fdaf9"
+      },
+      "execution_count": 7,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Name: Hyndhavi | Age: 25 | Height: 1.53 | Student: True\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "print (f\"Age in months: {Age*12}\")\n",
+        "print (f\"Age in days: {Age*365}\")\n",
+        "print (f\"The remainder when age is divided by 7: {Age % 7}\")\n",
+        "print(f\"Age raised to the power of 2: {Age ** 2}\")"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "5h2hCvZi5ZCN",
+        "outputId": "2e1bf961-ac48-413d-bfef-7e83f2c425de"
+      },
+      "execution_count": 12,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Age in months: 300\n",
+            "Age in days: 9125\n",
+            "The remainder when age is divided by 7: 4\n",
+            "Age raised to the power of 2: 625\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "Nwj4xr1752xe"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
